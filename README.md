693c6f9463595e04f5f2e8dfe26028f2f0b258cc
John Watson <john.watson@matillion.com>
2023-05-20 09:53:50 +0100

    feat: first pass basic golang api in mux

--- README.md
+++ README.md
@@ -1,2 +1,180 @@
-# artifact-flow-api
-Backend API for Artifact Flow
+# REST API for Artifact Records
+
+This is a REST API implemented in Golang that allows users to manage artifact records. The API supports basic CRUD operations (Create, Read, Update, Delete) for managing artifact data.
+
+## Requirements
+
+- Go 1.20 or above
+- MongoDB
+
+## Installation Locally
+
+1. Clone the repository:
+
+```
+git clone https://github.com/artifact-flow/artifact-flow-api.git
+```
+
+2. Change to the project directory:
+
+```
+cd artifact-flow-api
+```
+
+3. Install dependencies:
+
+```
+go mod download 
+```
+
+4. Configure MongoDB database locally:
+   
+```
+docker compose -f ./local-development/docker-compose-mongo.yml up -d
+```
+
+5. Run the application:
+
+```
+go run main.go
+```
+
+The API server should now be running on `http://localhost:8000`.
+
+## API Endpoints
+
+### Create an Artifact Record
+
+**Request**
+
+```
+POST /artifacts
+```
+
+Create a new artifact record.
+
+**Request Body**
+
+```json
+{
+  "name": "Artifact Name",
+  "description": "Artifact Description",
+  "category": "Artifact Category"
+}
+```
+
+**Response**
+
+```json
+{
+  "id": "123",
+  "name": "Artifact Name",
+  "description": "Artifact Description",
+  "category": "Artifact Category"
+}
+```
+
+### Get All Artifact Records
+
+**Request**
+
+```
+GET /artifacts
+```
+
+Retrieve all artifact records.
+
+**Response**
+
+```json
+[
+  {
+    "id": "123",
+    "name": "Artifact Name",
+    "description": "Artifact Description",
+    "category": "Artifact Category"
+  },
+  {
+    "id": "456",
+    "name": "Another Artifact",
+    "description": "Another Description",
+    "category": "Another Category"
+  }
+]
+```
+
+### Get a Specific Artifact Record
+
+**Request**
+
+```
+GET /artifacts/{id}
+```
+
+Retrieve a specific artifact record by its ID.
+
+**Response**
+
+```json
+{
+  "id": "123",
+  "name": "Artifact Name",
+  "description": "Artifact Description",
+  "category": "Artifact Category"
+}
+```
+
+### Update an Artifact Record
+
+**Request**
+
+```
+PUT /artifacts/{id}
+```
+
+Update an existing artifact record.
+
+**Request Body**
+
+```json
+{
+  "name": "Updated Artifact Name",
+  "description": "Updated Artifact Description",
+  "category": "Updated Artifact Category"
+}
+```
+
+**Response**
+
+```json
+{
+  "id": "123",
+  "name": "Updated Artifact Name",
+  "description": "Updated Artifact Description",
+  "category": "Updated Artifact Category"
+}
+```
+
+### Delete an Artifact Record
+
+**Request**
+
+```
+DELETE /artifacts/{id}
+```
+
+Delete an existing artifact record.
+
+**Response**
+
+```
+Artifact record deleted successfully.
+```
+
+## Contributing
+
+Contributions are welcome! If you find any issues or want to add new features, please submit a pull request.
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
---
+++ main.go
@@ -0,0 +1,160 @@
+// main.go
+
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+// Artifact represents an artifact record
+type Artifact struct {
+	ID          primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
+	Name        string             `json:"name,omitempty" bson:"name,omitempty"`
+	Description string             `json:"description,omitempty" bson:"description,omitempty"`
+	Category    string             `json:"category,omitempty" bson:"category,omitempty"`
+}
+
+// Connection string for MongoDB
+const connectionString = "mongodb://localhost:27017"
+
+// Database Name
+const dbName = "artifactdb"
+
+// Collection name
+const collName = "artifacts"
+
+// MongoDB client
+var client *mongo.Client
+
+// Create an artifact record
+func createArtifact(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	var artifact Artifact
+	_ = json.NewDecoder(r.Body).Decode(&artifact)
+
+	collection := client.Database(dbName).Collection(collName)
+	result, err := collection.InsertOne(r.Context(), artifact)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	artifact.ID = result.InsertedID.(primitive.ObjectID)
+	json.NewEncoder(w).Encode(artifact)
+}
+
+// Get all artifact records
+func getArtifacts(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	var artifacts []Artifact
+
+	collection := client.Database(dbName).Collection(collName)
+	cursor, err := collection.Find(r.Context(), bson.M{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer cursor.Close(r.Context())
+	for cursor.Next(r.Context()) {
+		var artifact Artifact
+		cursor.Decode(&artifact)
+		artifacts = append(artifacts, artifact)
+	}
+
+	json.NewEncoder(w).Encode(artifacts)
+}
+
+// Get a specific artifact record
+func getArtifact(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	params := mux.Vars(r)
+	id, _ := primitive.ObjectIDFromHex(params["id"])
+
+	var artifact Artifact
+
+	collection := client.Database(dbName).Collection(collName)
+	err := collection.FindOne(r.Context(), bson.M{"_id": id}).Decode(&artifact)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	json.NewEncoder(w).Encode(artifact)
+}
+
+// Update an artifact record
+func updateArtifact(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	params := mux.Vars(r)
+	id, _ := primitive.ObjectIDFromHex(params["id"])
+
+	var artifact Artifact
+	_ = json.NewDecoder(r.Body).Decode(&artifact)
+
+	collection := client.Database(dbName).Collection(collName)
+	update := bson.M{
+		"$set": bson.M{
+			"name":        artifact.Name,
+			"description": artifact.Description,
+			"category":    artifact.Category,
+		},
+	}
+	_, err := collection.UpdateOne(r.Context(), bson.M{"_id": id}, update)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	artifact.ID = id
+	json.NewEncoder(w).Encode(artifact)
+}
+
+// Delete an artifact record
+func deleteArtifact(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	params :=
+
+ mux.Vars(r)
+	id, _ := primitive.ObjectIDFromHex(params["id"])
+
+	collection := client.Database(dbName).Collection(collName)
+	_, err := collection.DeleteOne(r.Context(), bson.M{"_id": id})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	json.NewEncoder(w).Encode("Artifact record deleted successfully.")
+}
+
+func main() {
+	// Initialize MongoDB client
+	clientOptions := options.Client().ApplyURI(connectionString)
+	var err error
+	client, err = mongo.Connect(nil, clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Initialize router
+	router := mux.NewRouter()
+
+	// API endpoints
+	router.HandleFunc("/artifacts", createArtifact).Methods("POST")
+	router.HandleFunc("/artifacts", getArtifacts).Methods("GET")
+	router.HandleFunc("/artifacts/{id}", getArtifact).Methods("GET")
+	router.HandleFunc("/artifacts/{id}", updateArtifact).Methods("PUT")
+	router.HandleFunc("/artifacts/{id}", deleteArtifact).Methods("DELETE")
+
+	// Start the server
+	log.Fatal(http.ListenAndServe(":8000", router))
+}
